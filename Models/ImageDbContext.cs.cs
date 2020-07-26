2c04664a86ca8b0629f72c7dba296214740bf8a9
Zdravko Kovachev <DJ-ZdravoMIX@users.noreply.github.com>
2020-07-26 16:29:24 +0300

    Add project files.

---
+++ Models/ImageDbContext.cs.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace Kursova_Web_Gallery.Models
+{
+   
+        public class ImageDbContext : DbContext
+        {
+            public ImageDbContext(DbContextOptions<ImageDbContext> options) : base(options)
+            { }
+
+            public DbSet<ImageModel> Images { get; set; }
+        }
+
+    }
+
---
+++ Models/ImageModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Kursova_Web_Gallery.Models
+{
+    public class ImageModel
+    {
+        [Key]
+        public int ImageId { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Title { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Image Name")]
+        public string ImageName { get; set; }
+
+        [NotMapped]
+        [DisplayName("Upload File")]
+        public IFormFile ImageFile { get; set; }
+    }
+
+}
